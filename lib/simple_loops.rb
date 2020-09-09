a9594420ebe9df93609b3888f54fbb72f5ba1ef0
Kevin Botero <kbotero201@gmail.com>
2020-09-09 16:54:10 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -22,3 +22,12 @@ def output_array(array)
     count += 1
   end
 end
+
+ef return_string_array(array)
+  count = 0
+  while count < array.length do
+    array[count] = array[count].to_s
+    count += 1
+  end
+  array
+end
